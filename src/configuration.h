63103470d72870ca4183fcfa75b3dba0ec86ddc7
akirasan <akirasan21h@gmail.com>
2020-08-21 10:23:28 +0200

    configfile

---
+++ src/configuration.h
@@ -0,0 +1,6 @@
+// Configuración datos WiFi y servidor MQTT   
+
+const char *ssid = "XXXXXXXX";               // SSID de la conexión WiFi
+const char *password = "XXXXXXX";            // Password conexión WiFi
+const char *mqtt_server = "XXX.XXX.XXX.XXX"; // Dirección IP del servidor MQTT
+const uint16_t mqtt_server_port = 1883;      // Puerto de conexión al servidor
